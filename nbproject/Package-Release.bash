ddcccbb861f880fc7d9366520f4bd5548600378d
anthony campbell <anthony@minnow.eu>
2016-08-03 10:58:02 +0100

    example for stackoverflow

--- nbproject/Package-Release.bash
+++ nbproject/Package-Release.bash
@@ -13,9 +13,9 @@ CND_BUILDDIR=build
 CND_DLIB_EXT=dll
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/perfectforwarding
-OUTPUT_BASENAME=perfectforwarding
-PACKAGE_TOP_DIR=perfectforwarding/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/pointertomember
+OUTPUT_BASENAME=pointertomember
+PACKAGE_TOP_DIR=pointertomember/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/perfectforwarding/bin"
+makeDirectory "${NBTMPDIR}/pointertomember/bin"
 copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/perfectforwarding.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/pointertomember.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/perfectforwarding.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/pointertomember.tar *
 checkReturnCode
 
 # Cleanup
